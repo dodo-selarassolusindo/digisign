633a76fd4bd2f6c8f62b1a43997c086332ed54d9
dodo-selarassolusindo <119738003+dodo-selarassolusindo@users.noreply.github.com>
2023-02-23 14:31:51 +0700

    .

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -59,7 +59,7 @@
                     <!-- User Account: style can be found in dropdown.less -->
                     <li class="dropdown user user-menu">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <img src="<?php echo base_url('resources/img/user2-160x160.jpg');?>" class="user-image" alt="User Image">
+                            <img src="<?php echo base_url('resources/img/user2-160x160.jpeg');?>" class="user-image" alt="User Image">
                             <span class="hidden-xs"><?=$this->session->userdata["your_session_prefix_nama"]?></span>
                         </a>
                         <ul class="dropdown-menu">
@@ -93,7 +93,7 @@
             <!-- Sidebar user panel -->
             <div class="user-panel">
                 <div class="pull-left image">
-                    <img src="<?php echo base_url('resources/img/user2-160x160.jpg');?>" class="img-circle" alt="User Image">
+                    <img src="<?php echo base_url('resources/img/user2-160x160.jpeg');?>" class="img-circle" alt="User Image">
                 </div>
                 <div class="pull-left info">
                     <p><?=$this->session->userdata["your_session_prefix_nama"]?></p>
